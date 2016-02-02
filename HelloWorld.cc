15e11d25e85ba6b6f976202af1ffde40bb43ad1d
Leonard Chiang <leonardchiang@storm.cis.fordham.edu>
2016-02-01 21:12:09 -0500

    commit

---
+++ HelloWorld.cc
@@ -0,0 +1,19 @@
+// cin with multiple  strings
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+  string a;
+  cin >> a;
+
+  string b;
+  cin >> b;
+
+  cout << "Hello " << a << endl;
+  cout << "Hello " << b << endl;
+}
+
+
+# CISC1600
