2959a76b81cf442d32bd3ca438ce6f024c768980
llego2019 <lchiang@fordham.edu>
2016-02-08 16:30:46 -0500

    Update HelloWorld.cc

--- HelloWorld.cc
+++ HelloWorld.cc
@@ -6,13 +6,22 @@ using namespace std;
 int main ()
 {
   string a;
-  cin >> a;
-
+  cout << ""Hi, my name is ___."" << endl;
+  cout << "(What is your name?)" << endl;
+  cin >> x;
+  cout << ""Hi, my name is "" << x << "." << endl;
+  
   string b;
-  cin >> b;
-
-  cout << "Hello " << a << endl;
-  cout << "Hello " << b << endl;
+  cout << ""I am ___ years old."" << endl;
+  cout << "(What is your age?)" << endl;
+  cin >> y;
+  cout << ""I am "" << y << " years old, and ";
+  
+  string c;
+  cout << "my favorite thing is ___."" << endl;
+  cout << "(What is your favorite thing?)" << endl;
+  cin >> z;
+  cout << "my favorite thing is "" << z << "." << endl;
 }
 
 
