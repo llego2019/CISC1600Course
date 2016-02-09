1e012756d1bc53ffa37403c012c0340aa315d727
llego2019 <lchiang@fordham.edu>
2016-02-08 20:29:47 -0500

    Create Assignment3.cc

---
+++ Assignment3.cc
@@ -0,0 +1,32 @@
+// Assignment #3
+// Using new variable types
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main ()
+{
+  string x;
+  string y;
+  string z;
+
+  cout << "What is your name?" << endl;
+  cin >> x;
+  cout << "Okay, now I know your name is " << x << "." << endl;
+
+  cout << "What is your age?" << endl;
+  cin >> y;
+  cout << "Okay, now I know your age is " << y << "." << endl;
+
+  cout << "What is your favorite thing?" << endl;
+  cin >> z;
+  cout << "Okay, now I know your favorite thing is " << z << "."  << endl;
+
+  cout << "So, if you wanted to introduce yourself, you could say: 'Hi, my name is " << x << ". " << endl;
+  cout << "I am " << y << " years old, and my favorite thing is " << z << ".'" << endl;
+  
+  }
+  
+  #CISC1600
